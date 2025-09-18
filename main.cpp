e5815d50c76aefd74d0e4cc3adb46cff1cae5da0
Lysdal <lysdal1234@gmail.com>
2025-09-18 12:37:51 +0200

    aa

--- main.cpp
+++ main.cpp
@@ -194,168 +194,7 @@ using namespace std;
 int num_of_thread = 8;
 
 //parallel peeling for a given k, advanced version
-vector<int> Parpeel(GraphCPU& g, int k, vector<int> kBin, int* vert){
-	unsigned n = g.V;
-
-    //获取线程数
-    int NUM_THREADS = num_of_thread;
-    // omp_set_num_threads(NUM_THREADS);
-
-    #pragma omp parallel
-    {
-        #pragma omp master
-        NUM_THREADS = omp_get_num_threads();
-    }
-
-    int visited = 0;
-
-    // printf("number of threads: %d, k: %d.\n", NUM_THREADS,k);
-
-
-    //重新写一个缩图
-    std::vector<int> iH, OC, flag, shellVert;
-    iH.resize(n);
-    OC.resize(n);
-    flag.resize(n,0);
-
-    for (int i = 0; i < n; i++) {
-        OC[i] = g.outDegrees[i];
-        iH[i] = g.inDegrees[i];
-    }
-
-    //去除shell小于k的顶点,不在shell中的out-core number为0
-    for(int i=kBin[k];i<n;i++)
-        shellVert.push_back(vert[i]);
-
-    for(int i = 0;i<kBin[k];i++){
-        flag[vert[i]] = 1;
-        OC[vert[i]] = 0;
-    }
-
-    //更新shellVert中顶点的degree
-    for(int i=kBin[k];i<n;i++){
-        int v = vert[i];
-        for (int l = 0; l < g.outEdges[v].size(); l++) {
-            if (flag[g.outEdges[v][l]]) OC[v]--;
-        }
-        for (int k = 0; k < g.inEdges[v].size(); k++) {
-            if (flag[g.inEdges[v][k]]) iH[v]--;
-        }
-    }
-
-    int n1 = shellVert.size();
-
-
-    //每个线程运行这部分代码
-#pragma omp parallel
-{
-    int level = 0;
-
-    long buff_size = n;
-
-    int *buff = (int *)malloc(buff_size*sizeof(int));
-    assert(buff != NULL);
-
-    int start = 0, end = 0;
-
-    while(visited < n1){
-        //获取out-degree为level的顶点, 或in-degree 小于k的顶点
-        #pragma omp for schedule(static)
-        for(int i = 0; i < n1; i++){
-            int u = shellVert[i];
-            if(flag[u] < 1){
-                if(OC[u] == level){
-                    buff[end] = u;
-                    end++;
-                    flag[u]++;
-                }
-                else if (iH[u] < k){
-                    OC[u] = level;
-                    buff[end] = u;
-                    end++;
-                    flag[u]++;
-                }
-            }
-        }
-
-        //处理buff中的顶点, 需要给小于din小于k的顶点加一个flag, 首先初始化所有顶点的flag为0,如果小于k, 那就将其修改为1, 并将其放入buff中
-        while(start < end){
-            int v = buff[start];
-            start++;
-            flag[v]++;
-
-            //处理in-degree小于k的顶点
-            for(int j = 0; j < g.outEdges[v].size(); j++){
-                int u = g.outEdges[v][j];
-
-                if(flag[u]) continue;
-
-                // int din_u = __sync_fetch_and_sub(&iH[u], 1);
-                int din_u = __sync_fetch_and_sub(&iH[u], 1);
-
-                //in-degree小于k的顶点, 其l-number为当前的level
-                if(din_u <= k){
-                    if(flag[u] == 0){
-                        buff[end] = u;
-                        end++;
-
-                        OC[u] = level;
-                        flag[u]++;
-                    }
-                }
-
-            }
-
-            //处理out-degree不大于level的顶点
-            for(int j = 0; j < g.inEdges[v].size(); j++){
-                int u = g.inEdges[v][j];
-
-                if(flag[u]) continue;
-
-                int dout_u = OC[u];
-
-                if(dout_u > level){
-                    int du = __sync_fetch_and_sub(&OC[u], 1);
-                    //将减小后degree等于level的顶点放在buff末尾
-                    if(du==(level+1)){
-                        buff[end] = u;
-                        end++;
-                        flag[u]++;
-                    }
-
-                    if(du <= level){
-                        __sync_fetch_and_add(&OC[u], 1);
-                        flag[u]++;
-                    }
-                }
-
-            }
-
-            // printf("start: %d, end: %d.\n",start,end);
-        }
-
-        //减去处理过的顶点数
-        __sync_fetch_and_add(&visited, end);
-
-
-        // printf("end: %d, n1: %d.\n",end, n1);
-        // printf("level: %d, visited: %d.\n",level, visited);
-
-        #pragma omp barrier
-        start = 0;
-        end = 0;
-        level = level+1;
-
-    }
-
-    free( buff );
-}
-
-    return OC;
-}
-
-//parallel peeling for a given k, advanced version
-vector<int> Parpeel(GraphCPU& g, int k, vector<int> upper, vector<int> kBin, int* vert) {
+vector<int> Parpeel(GraphCPU& g, int k, vector<int> upper, vector<int> kBin, vector<int> vert) {
 	unsigned int n = g.V;
     int NUM_THREADS = omp_get_max_threads();
 
@@ -508,56 +347,56 @@ vector<int> Parpeel(GraphCPU& g, int k, vector<int> upper, vector<int> kBin, int
 }
 
 
-vector<int> kshell(GraphCPU& g, vector<int> iH, vector<int>& kBin, int* vert){
+vector<int> kshell(GraphCPU& g, vector<int> dIn, vector<int>& kBin, vector<int>& vert){
 	unsigned n = g.V;
 	vector<int> pos;
 
 	pos.resize(n+1);
-	// vert.resize(n);
+	vert.resize(n);
 	kBin.resize(n);
 
-	for (int i = 0; i < n; i++) {
-		kBin[iH[i]] += 1;
+	for (unsigned v = 0; v < n; v++) {
+		kBin[dIn[v]] += 1;
 	}
 
-	int level = *max_element(iH.begin(),iH.end());
+	int kUpper = *max_element(dIn.begin(),dIn.end());
 
 	int start = 0;
-	for (int i = 0; i <= level; i++) {
+	for (int i = 0; i <= kUpper; i++) {
 		int num = kBin[i];
 		kBin[i] = start;
 		start += num;
 	}
-	for (int i = 0; i < n; i++) {
-		pos[i] = kBin[iH[i]];
-		vert[pos[i]] = i;
-		kBin[iH[i]] += 1;
+	for (unsigned v = 0; v < n; v++) {
+		pos[v] = kBin[dIn[v]];
+		vert[pos[v]] = v;
+		kBin[dIn[v]] += 1;
 	}
-	for (int i = level; i > 0; i--) {
+	for (int i = kUpper; i > 0; i--) {
 		kBin[i] = kBin[i - 1];
 	}
 	kBin[0] = 0;
 
 
 	int record = -1;
-	int* result = new int [n];
+	vector<int> result(n);
 	int resultPos = 0;
-	for(int i = 0; i< n;i++){
-		if(iH[vert[i]] != record){
-			result[resultPos] = iH[vert[i]];
+	for (int i = 0; i < n; i++){
+		if (dIn[vert[i]] != record){
+			result[resultPos] = dIn[vert[i]];
 			resultPos ++;
-			record  = iH[vert[i]];
+			record = dIn[vert[i]];
 		}
 	}
 	result[0] = -1;
 	int maxK = record;
-	for(int i = 1;i<resultPos;i++){
-		result[i] = result[i-1]+1;
+	for (int i = 1; i < resultPos; i++){
+		result[i] = result[i-1] + 1;
 	}
 	result[resultPos] = maxK;
 
 	std::vector<int> res;
-	for(int i=1;i<resultPos+1;i++){
+	for (int i = 1; i < resultPos + 1; i++){
 		res.push_back(result[i]);
 	}
 
@@ -571,28 +410,18 @@ void PDC(GraphCPU& g){
     //获取线程数
     int NUM_THREADS = num_of_thread;
     omp_set_num_threads(NUM_THREADS);
-    // #pragma omp parallel
-    // {
-    //     #pragma omp master
-    //     NUM_THREADS = omp_get_num_threads();
-    // }
 
+
+	// find out k-max
     int vis_num = 0;
 
-    std::vector<int> iH, OC;
-    iH.resize(n);
-    OC.resize(n);
+    std::vector<int> dIn, dOut;
+    dIn.resize(n);
+    dOut.resize(n);
     for (int i = 0; i < n; i++) {
-        OC[i] = g.outDegrees[i];
-        iH[i] = g.inDegrees[i];
+        dOut[i] = g.outDegrees[i];
+        dIn[i] = g.inDegrees[i];
     }
-
-    // printf("number of threads: %d.\n", NUM_THREADS);
-
-
-    // printf("in-deg size: %d, n: %d.\n",iH.size(),n);
-
-
 
     //每个线程运行这部分代码
 #pragma omp parallel
@@ -601,28 +430,20 @@ void PDC(GraphCPU& g){
 
     long buff_size = n;
 
-    // printf("buff size: %d.\n", buff_size);
-
     int *buff = (int *)malloc(buff_size*sizeof(int));
     assert(buff != NULL);
 
     int start = 0, end = 0;
 
     while(vis_num < n){
-
-        // printf("test.\n");
-
         //获取in-degree为level的顶点
         #pragma omp for schedule(static)
         for(int i = 0; i < n; i++){
-            // printf("dddd.\n");
-            if(iH[i] == level){
+            if(dIn[i] == level){
                 buff[end] = i;
                 end++;
             }
         }
-
-        // printf("end: %d.\n", end);
 
         //处理buff中的顶点
         while(start < end){
@@ -631,17 +452,17 @@ void PDC(GraphCPU& g){
 
             for(int j = 0; j < g.outEdges[v].size(); j++){
                 int u = g.outEdges[v][j];
-                int din_u = iH[u];
+                int din_u = dIn[u];
 
                 if(din_u > level){
-                    int du = __sync_fetch_and_sub(&iH[u], 1);
+                    int du = __sync_fetch_and_sub(&dIn[u], 1);
                     //将下一个level的顶点放在buff末尾
                     if(du==(level+1)){
                         buff[end] = u;
                         end++;
                     }
 
-                    if(du <= level) __sync_fetch_and_add(&iH[u], 1);
+                    if(du <= level) __sync_fetch_and_add(&dIn[u], 1);
                 }
 
             }
@@ -654,44 +475,40 @@ void PDC(GraphCPU& g){
         start = 0;
         end = 0;
         level = level+1;
-
-        // printf("rest: %d.\n",n-vis_num);
-        // printf("level: %d.\n", level);
-
     }
-
 
     free(buff);
 }
 
-    // printf("peeling done.\n");
+    int level = *max_element(dIn.begin(),dIn.end());
 
-    int level = *max_element(iH.begin(),iH.end());
+	cout << "k-max: " << level << endl;
 
     //找出k-shell对应的k
 	vector<int> kBin;
-	int* vert = (int *)malloc(n * sizeof(int));
+	vector<int> vert;
 
-    vector<int> res;
-    res = kshell(g, iH, kBin, vert);
+    vector<int> kshells;
+    kshells = kshell(g, dIn, kBin, vert);
 
-	// for (auto r: res)
-	// 	cout << r << " ";
-	// cout << endl;
-
-    printf("kmax: %d, number of shells: %ld.\n", level, res.size());
+	cout << kshells.size() << " shells: ";
+	for (auto s: kshells) {
+		cout << "(k," << s << ") ";
+	}
+	cout << endl;
 
 
     //对于shell中的每个k, 调用l的分解算法
     std::vector<std::vector<int>> Fres;
 
-    // for (auto k : res) {
-    //     Fres.push_back(Parpeel(k));
-    // }
-    Fres.push_back(Parpeel(g, 0, kBin, vert));
-    // Fres.push_back(Parpeel(1));
-    for(int i=1; i<res.size(); i++){
-        Fres.push_back(Parpeel(g, res[i],Fres.back(), kBin, vert));
+	vector<int> init(g.V);
+	for (int i = 0; i < g.V; i++)
+		init[i] = g.outDegrees[i];
+
+	Fres.push_back(Parpeel(g, 0, init, kBin, vert));
+
+    for(int i = 1; i < kshells.size(); i++){
+        Fres.push_back(Parpeel(g, kshells[i],Fres.back(), kBin, vert));
     }
 
     //write results: 1 row
@@ -708,8 +525,6 @@ void PDC(GraphCPU& g){
 	}
 
     printf("decomposition done.\n");
-
-	free(vert);
 }
 
 
